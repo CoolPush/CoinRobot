d48f7b0be6471a003d854ad3861fbad60a9e914e
Young Xu <1397190480@qq.com>
2021-02-15 01:26:05 +0800

    Update router.go

--- router.go
+++ router.go
@@ -5,7 +5,8 @@ import (
 )
 
 func initRouter() {
-	router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Recovery())
 	router.POST("/api", Handler)
 	log.Fatal(router.Run("127.0.0.1:21125"))
 }
